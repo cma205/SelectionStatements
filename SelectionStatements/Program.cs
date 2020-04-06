9178cc6873f073d2fc6293adfafc6ec305b9e886
Charles Alford <cm205@Charless-MacBook-Pro.local>
2020-04-06 09:53:31 -0500

    Number Game Finished

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my number game!");
+            Console.WriteLine("Try to guess my favorite number!");
+            int favNumber = 7;
+            var userInput = int.Parse(Console.ReadLine());
+            if (favNumber < userInput)
+            {
+                Console.WriteLine("Sorry, that's too high.");
+            }
+            else if (favNumber > userInput)
+            {
+                Console.WriteLine("Sorry, that's too low.");
+            }
+            else
+            {
+                Console.WriteLine("That's it! Congratulations!");
+            }
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
